b7aa951134c06bcd27d582226d113d0037d4add2
ipromise2324 <168allen55@gmail.com>
2023-06-07 01:10:50 +0800

    feat: add sendUserOperation and validateParameters

--- src/handler/userOpHandler.ts
+++ src/handler/userOpHandler.ts
@@ -9,18 +9,70 @@ import {
 import { BigNumber } from 'ethers'
 import { UserOpInterface } from '../interfaces/userOpInterface'
 import { Entrypoint } from '../contracts'
-import { requireCond, RpcError, deepHexlify } from '../utils/utils'
-
+import { requireCond, RpcError, deepHexlify, tostr } from '../utils/utils'
+import { BundlerConfig } from '../modules//BundlerConfig'
+import { resolveProperties } from '@ethersproject/properties'
+import { ExecutionManager } from '../modules/ExecutionManager'
 const HEX_REGEX = /^0x[a-fA-F\d]*$/i
 
 export class UserOpHandler implements UserOpInterface {
-    constructor(readonly entryPoint: Entrypoint) {}
-    validateParameters(
-        userOp: UserOperationStruct,
+    constructor(
+        readonly execManager: ExecutionManager,
+        readonly entryPoint: Entrypoint,
+        readonly config: BundlerConfig
+    ) {}
+    async validateParameters(
+        userOp1: UserOperationStruct,
         entryPointInput: string,
-        requireSignature: boolean,
-        requireGasParams: boolean
+        requireSignature = true,
+        requireGasParams = true
     ): Promise<boolean> {
+        requireCond(entryPointInput != null, 'No entryPoint param', -32602)
+
+        if (
+            entryPointInput?.toString().toLowerCase() !==
+            this.config.entryPoint.toLowerCase()
+        ) {
+            throw new Error(
+                `The EntryPoint at "${entryPointInput}" is not supported. This bundler uses ${this.config.entryPoint}`
+            )
+        }
+        // minimal sanity check: userOp exists, and all members are hex
+        requireCond(userOp1 != null, 'No UserOperation param')
+        const userOp = (await resolveProperties(userOp1)) as any
+
+        const fields = [
+            'sender',
+            'nonce',
+            'initCode',
+            'callData',
+            'paymasterAndData',
+        ]
+        if (requireSignature) {
+            fields.push('signature')
+        }
+        if (requireGasParams) {
+            fields.push(
+                'preVerificationGas',
+                'verificationGasLimit',
+                'callGasLimit',
+                'maxFeePerGas',
+                'maxPriorityFeePerGas'
+            )
+        }
+        fields.forEach((key) => {
+            requireCond(
+                userOp[key] != null,
+                'Missing userOp field: ' + key + JSON.stringify(userOp),
+                -32602
+            )
+            const value: string = userOp[key].toString()
+            requireCond(
+                value.match(HEX_REGEX) != null,
+                `Invalid hex value for property ${key}:${value} in UserOp`,
+                -32602
+            )
+        })
         throw new Error('Method not implemented.')
     }
     async estimateUserOperationGas(
@@ -36,10 +88,23 @@ export class UserOpHandler implements UserOpInterface {
         }
         throw new Error('Method not implemented.')
     }
-    sendUserOperation(
+    async sendUserOperation(
         userOp1: UserOperationStruct,
         entryPointInput: string
     ): Promise<string> {
+        await this.validateParameters(userOp1, entryPointInput)
+
+        const userOp = await resolveProperties(userOp1)
+
+        console.log(
+            `UserOperation: Sender=${userOp.sender}  Nonce=${tostr(
+                userOp.nonce
+            )} EntryPoint=${entryPointInput} Paymaster=${getAddr(
+                userOp.paymasterAndData
+            )}`
+        )
+        await this.execManager.sendUserOperation(userOp, entryPointInput)
+        return await this.entryPoint.getUserOpHash(userOp)
         throw new Error('Method not implemented.')
     }
     async getOperationEvent(
