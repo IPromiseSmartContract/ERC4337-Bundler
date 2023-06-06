bd99f9234373e1fe07e144aa8690bfd7cde57545
super <super890831@gmail.com>
2023-06-06 16:57:34 +0800

    feat: add requireCond

---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+export class RpcError extends Error {
+    // error codes from: https://eips.ethereum.org/EIPS/eip-1474
+    constructor(
+        msg: string,
+        readonly code?: number,
+        readonly data: any = undefined
+    ) {
+        super(msg)
+    }
+}
+
+export function requireCond(
+    cond: boolean,
+    msg: string,
+    code?: number,
+    data: any = undefined
+): void {
+    if (!cond) {
+        throw new RpcError(msg, code, data)
+    }
+}
